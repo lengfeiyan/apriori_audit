ac1efea5f310b11bf89fb821e954cb57e421e4be
lengfeiyan <lengfeiyan@gmail.com>
2017-12-05 16:24:12 +0800

    Create apriori_audit.py

---
+++ apriori_audit.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+本文件根据《机器学习实战》第十一章代码略作修改
+Created on Tue Dec 05 09:28:53 2017
+
+@author: dushu
+"""
+from numpy import *
+
+
+def loadDataSet():
+    dataSet = []
+    fr = open('testFile.txt')
+    for line in fr.readlines():
+        line = line.replace('\n', '')
+        companies = line.split(',')
+        dataSet.append(companies)
+    return dataSet
+
+
+def createC1(dataSet):
+    C1 = []
+    for transaction in dataSet:
+        for item in transaction:
+            if not [item] in C1:
+                C1.append([item])
+    C1.sort()
+    return map(frozenset, C1)
+
+
+def scanD(D, Ck, minSupport):
+    ssCnt = {}
+    for tid in D:
+        for can in Ck:
+            if can.issubset(tid):
+                if can not in ssCnt:
+                    ssCnt[can] = 1
+                else:
+                    ssCnt[can] += 1
+    numItems = float(len(D))
+    retList = []
+    supportData = {}
+    for key in ssCnt:
+        support = ssCnt[key]/numItems
+        if support >= minSupport:
+            retList.insert(0, key)
+        supportData[key] = support
+    return retList, supportData
+
+
+def aprioriGen(Lk, k):
+    retList = []
+    lenLk = len(Lk)
+    for i in range(lenLk):
+        for j in range(i+1, lenLk):
+            L1 = list(Lk[i])[:k-2]
+            L2 = list(Lk[j])[:k-2]
+            L1.sort()
+            L2.sort()
+            if L1 == L2:
+                retList.append(Lk[i] | Lk[j])
+    return retList
+
+
+def apriori(dataSet, minSupport=0.5):
+    C1 = createC1(dataSet)
+    D = map(set, dataSet)
+    L1, supportData = scanD(D, C1, minSupport)
+    L = [L1]
+    k = 2
+    while (len(L[k-2]) > 0):
+        Ck = aprioriGen(L[k-2], k)
+        Lk, supK = scanD(D, Ck, minSupport)
+        supportData.update(supK)
+        L.append(Lk)
+        k += 1
+    return L, supportData
+
+
+def generateRules(L, supportData, minConf=0.7):
+    bigRuleList = []
+    for i in range(1, len(L)):
+        for freqSet in L[i]:
+            H1 = [frozenset([item]) for item in freqSet]
+            if (i > 1):
+                rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf)
+            else:
+                calcConf(freqSet, H1, supportData, bigRuleList, minConf)
+    return bigRuleList
+
+
+def calcConf(freqSet, H, supportData, brl, minConf=0.7):
+    prunedH = []
+    for conseq in H:
+        conf = supportData[freqSet]/supportData[freqSet-conseq]
+        if conf >= minConf:
+            brl.append((freqSet-conseq, conseq, conf))
+            prunedH.append(conseq)
+    return prunedH
+
+
+def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
+    m = len(H[0])
+    if (len(freqSet) > (m + 1)):
+        Hmp1 = aprioriGen(H, m+1)
+        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
+        if (len(Hmp1) > 1):
+            rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
+
+
+def pntRules(ruleList):
+    for ruleTup in ruleList:
+        for item in ruleTup[0]:
+            print item
+        print "           -------->"
+        for item in ruleTup[1]:
+            print item
+        print "可信度: %f" % ruleTup[2]
+        print
+
+
+if "__main__" == __name__:
+    dataSet = loadDataSet()
+    L, supportData = apriori(dataSet, 0.2)
+    rule = generateRules(L, supportData)
+    pntRules(rule)
